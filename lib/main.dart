b32592e94a5fc09a78112a39e91ece2a9638c7c3
moghoneim-code <mghoneam7@gmail.com>
2023-12-07 17:21:01 +0200

    fix bugs and push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // // main.dart
-// import 'package:demo_project/model/user_events_viewmodel.dart';
-// import 'package:demo_project/viewmodel/user_events_viewmodel.dart';
+// import 'package:demo_project/model/api_model.dart';
+// import 'package:demo_project/viewmodel/api_viewmodel.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 // import 'package:provider/provider.dart';
@@ -55,10 +55,10 @@
 //     );
 //   }
 // }
-import 'package:demo_project/view/view.dart';
-import 'package:demo_project/viewmodel/api_viewmodel.dart';
+import 'package:demo_project/model/reservation_model.dart';
 import 'package:demo_project/view/my_home_page.dart';
 import 'package:demo_project/view/reservation.dart';
+import 'package:demo_project/viewmodel/api_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
@@ -76,11 +76,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ApiViewModel()),
-
       ],
       child: AdaptiveTheme(
         light: ThemeData(
@@ -91,14 +89,14 @@ class MyApp extends StatelessWidget {
         ),
         initial: savedThemeMode ?? AdaptiveThemeMode.light,
         builder: (theme, darkTheme) => MaterialApp(
-            theme: theme,
-            darkTheme: darkTheme,
-            home: const MyHomePage(),
-            debugShowCheckedModeBanner: false,
-            routes: {
-              ReservationScreen.routeName: (context) => const ReservationScreen(),
-              AppScreen.routeName: (context) => const AppScreen(),
-            }),
+          routes: {
+            ReservationScreen.routeName: (context) => const ReservationScreen(),
+          },
+          theme: theme,
+          darkTheme: darkTheme,
+          home: const MyHomePage(),
+          debugShowCheckedModeBanner: false,
+        ),
       ),
     );
   }
--- lib/view/reservation.dart
+++ lib/view/reservation.dart
@@ -4,27 +4,44 @@ import 'package:demo_project/constants/list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../model/reservation_model.dart';
 import '../viewmodel/api_viewmodel.dart';
 
-class ReservationScreen extends StatelessWidget {
+class ReservationScreen extends StatefulWidget {
   const ReservationScreen({super.key});
 
   static const routeName = 'ReservationScreen';
+
+  @override
+  State<ReservationScreen> createState() => _ReservationScreenState();
+}
+
+class _ReservationScreenState extends State<ReservationScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(seconds: 0))
+        .then((value) => context.read<ApiViewModel>().getUserEvents());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final bool _isDark = AdaptiveTheme.of(context).isDefault;
     SizeScreen sizeScreen = SizeScreen(context);
     Size size = sizeScreen.getScreenSize;
-        return SafeArea(
-          child: Scaffold(
-      body: Padding(
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Consumer<ApiViewModel>(
-            builder: (context,T,child) {
-              final apiViewModel = Provider.of<ApiViewModel>(context);
+          child: Consumer<ApiViewModel>(builder: (context, provider, child) {
+            if (provider.viewState == ViewState.loading) {
+              return const Center(child: CircularProgressIndicator.adaptive());
+            }
+            if (provider.viewState == ViewState.loaded) {
               return ListView(
                 children: [
+                  _staysWidget(provider.reservations.first.stays!),
                   Image.asset(
                     'assets/images/img.png',
                     fit: BoxFit.cover,
@@ -39,8 +56,7 @@ class ReservationScreen extends StatelessWidget {
                     true,
                   ),
                   _textWidget(
-                  apiViewModel.reservations.first.startDate.toString()
-                    ,
+                    provider.reservations.first.stays!.first.name,
                     _isDark ? Colors.black : Colors.grey,
                     15,
                     false,
@@ -51,7 +67,8 @@ class ReservationScreen extends StatelessWidget {
                   Expanded(
                     child: GridView.builder(
                       shrinkWrap: true,
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
                         crossAxisSpacing: 50.0,
                         mainAxisSpacing: 0.2,
@@ -84,6 +101,7 @@ class ReservationScreen extends StatelessWidget {
                     20,
                     true,
                   ),
+                  
                   InkWell(
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -93,7 +111,7 @@ class ReservationScreen extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             _textWidget(
-                             '', // apiViewModel.reservations.first.stays
+                              '', // apiViewModel.reservations.first.stays
                               //     ?.map((e) => e.name),
                               Colors.white,
                               20,
@@ -111,18 +129,26 @@ class ReservationScreen extends StatelessWidget {
                         ),
                         Image.asset(
                           'assets/images/img.png',
-                            // apiViewModel.reservations.first.stays
-                            // ?.map((e) => e.stayImages?.first) as String),
-                        )],
+                          // apiViewModel.reservations.first.stays
+                          // ?.map((e) => e.stayImages?.first) as String),
+                        )
+                      ],
                     ),
                   ),
                 ],
               );
             }
-          ),
+            return Container();
+          }),
+        ),
       ),
-    ),
-        );
+    );
+  }
+  
+  Widget _staysWidget(List<Stay> stays){
+    return ListView(
+      shrinkWrap: true,
+    );
   }
 
   Widget _textWidget(var text, color, double textSize, bool isTitle) {
